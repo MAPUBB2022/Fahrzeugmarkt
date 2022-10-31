0f08cf1e76260f659b1edc2be00764e014998e3c
Andrei Gălițianu <andrei@galitia.nu>
2022-10-31 19:04:23 +0200

    implemented the inMemoryUserRepo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import view.View;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        View view = new View();
+        view.login();
     }
 }
