59b5cd05e342e93ec921e8e7fb7e12a2cfb621fb
atletul <46723813+vladiordache14@users.noreply.github.com>
2022-11-03 20:15:09 +0200

    Update View.java

--- src/view/View.java
+++ src/view/View.java
@@ -1,15 +1,14 @@
 package view;
 
 import controller.Controller;
-import model.Advert;
-import model.Car;
-import model.Motorcycle;
+import model.*;
+import repository.memory_repo.InMemoryUserRepository;
 
 import java.util.Scanner;
 
 public class View {
     private Controller controller;
-
+    private InMemoryUserRepository inMemoryUserRepository;
     private int userMode;
 
     public View() {
@@ -19,12 +18,45 @@ public class View {
 
     public void login()
     {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username:");
+        String username=myObj.nextLine();
+
+        System.out.println("Enter password:");
+        String password=myObj.nextLine();
+
+        User user= inMemoryUserRepository.findByUserAnsPass(username,password);
+        if (user instanceof Buyer){
+            userMode = 1;
+        }
+        if(user instanceof Seller) {
+            userMode = 2;
+        }
+        else {
+            userMode=0;//Admin
+        }
+
 
     }
 
     public void mainMenu()
     {
+        while (true) {
+            if (userMode == -1)
+                login();
+            if(userMode==0)//Admin
+            {
 
+
+            }
+            if(userMode==1){//Buyer
+
+            }
+            if(userMode==2){//Seller
+
+            }
+
+        }
     }
 
     public Advert createAd(){
@@ -36,18 +68,25 @@ public class View {
 
         System.out.println("make:");
         String make = myObj.nextLine();
+
         System.out.println("model:");
         String model = myObj.nextLine();
+
         System.out.println("year:");
         int year = Integer.parseInt(myObj.nextLine());
+
         System.out.println("displacement:");
         int displacement = Integer.parseInt(myObj.nextLine());
+
         System.out.println("hp:");
         int hp = Integer.parseInt(myObj.nextLine());
+
         System.out.println("torque:");
         int torque = Integer.parseInt(myObj.nextLine());
+
         System.out.println("used:");
         boolean used= Boolean.parseBoolean(myObj.nextLine());
+
         System.out.println("automatic gearbox:");
         boolean automaticGearbox= Boolean.parseBoolean(myObj.nextLine());
 
