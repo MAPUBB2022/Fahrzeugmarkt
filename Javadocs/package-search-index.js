6b58485b249984f5963abac5a27431d75129a2f3
Andrei Gălițianu <andrei@galitia.nu>
2022-11-29 09:57:50 +0200

    generated Javadocs

---
+++ Javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"repository"},{"l":"repository.db_repo"},{"l":"repository.memory_repo"},{"l":"view"}];updateSearchResults();
