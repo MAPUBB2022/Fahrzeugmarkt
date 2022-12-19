91675ea427149ae3debccbf8cb801a58904bf5a7
Andrei Gălițianu <andrei@galitia.nu>
2022-12-20 00:26:05 +0200

    Delete Javadocs directory

--- Javadocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"repository"},{"l":"repository.db_repo"},{"l":"repository.memory_repo"},{"l":"view"}];updateSearchResults();
